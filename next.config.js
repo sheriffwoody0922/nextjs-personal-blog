bd92c6b1ebb3a7adb0852e3cd012ef19e5f70070
sheriffwoody0922 <codewarrior0922@gmail.com>
2021-12-20 04:54:34 +0300

    ..

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const securityHeaders = [
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN',
   },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  }
   //{
   //  key: 'X-Content-Type-Options',
   //  value: 'nosniff',
